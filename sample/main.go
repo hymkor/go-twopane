418405c0677f66f0a1ac806fa3078cc32e5f9e0e
HAYAMA_Kaoru <iyahaya@nifty.com>
2019-09-21 08:47:21 +0900

    Add new sample (./sample/main.go)

---
+++ sample/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/zetamatta/go-twopane"
+)
+
+type Row string
+
+func (r Row) Title() string {
+	return string(r)
+}
+
+func (r Row) Contents() []string {
+	return []string{"Contents of " + string(r)}
+}
+
+func main() {
+	rows := []twopane.Row{}
+	for i := 0; i < 100; i++ {
+		rows = append(rows, Row(fmt.Sprintf("%d", i)))
+	}
+	err := twopane.View{Rows: rows, Clear: true}.Run()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+}
