262ee3f8001af00d3874eedcd046a9adc0384258
HAYAMA_Kaoru <iyahaya@nifty.com>
2020-05-21 01:20:24 +0900

    Do not escape surrrogate-paird unicode at running on Windows Terminal

--- textfilter_windows.go
+++ textfilter_windows.go
@@ -2,13 +2,16 @@ package twopane
 
 import (
 	"fmt"
+	"os"
 	"strings"
 )
+
+var surrogatePairOk = os.Getenv("WT_SESSION") != "" && os.Getenv("WT_PROFILE_ID") != ""
 
 func textfilter(s string) string {
 	var buffer strings.Builder
 	for _, c := range s {
-		if c > 0xFFFF {
+		if !surrogatePairOk && c > 0xFFFF {
 			fmt.Fprintf(&buffer, "&#x%X;", c)
 		} else {
 			buffer.WriteRune(c)
